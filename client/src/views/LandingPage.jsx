7c49e9ac8959e7a352a0ff8acd18f08b8d9d52a2
Luis Cardona <cardona-luis@outlook.com>
2025-06-08 05:39:31 -0500

    Logo

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import logo from '../assets/img/logo.png';
 
 const LandingPage = () => {
     return (
         <div className="landing-page" style={{ textAlign: 'center', padding: '4rem' }}>
-            <h1>Welcome to DNA Apps</h1>
-            <p>Your one-stop solution for modern web applications.</p>
+            <img src={logo} alt="" />
             <button style={{ padding: '1rem 2rem', fontSize: '1rem', marginTop: '2rem' }}>
                 Get Started
             </button>
