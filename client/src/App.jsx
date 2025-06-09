8e54c240c41f784b5e10d2c5e724f301afbb6579
Luis Cardona <cardona-luis@outlook.com>
2025-06-09 12:16:48 -0500

    Router : staticConfig

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './views/LandingPage'
 import Footer from './components/Footer'
-
+import StudentSub from './views/StudentSub'
+import AccessSubPage from './views/AccessSubPage'
 
 function App() {
-
   return (
-    <>
-     <LandingPage />
-     <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/student-access-subscription" element={<StudentSub />} />
+        <Route path="/all-app-access-subscription" element={<AccessSubPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ client/src/views/StudentSub.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StudentSub = () => {
+    return (
+        <div>
+            <h1>Student Submission</h1>
+            {/* Add your component content here */}
+            <p>This is the student submission page where students can submit their work.</p>
+        </div>
+    );
+};
+
+export default StudentSub;
