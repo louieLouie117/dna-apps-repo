4ed6f879fc2d22b3b3d4827a66ebf040dc857290
Luis Cardona <cardona-luis@outlook.com>
2025-06-10 17:30:32 -0500

    AllAppAccessLayout

--- client/src/components/PageHeader.jsx
+++ client/src/components/PageHeader.jsx
@@ -4,7 +4,9 @@ import logo from '../assets/img/logo.png'; // Adjust the path as needed
 const PageHeader = () => (
     <div className="logo">
         <h1 className="hidden">Project DNA Apps</h1>
-        <img src={logo} alt="Project DNA Logo" />
+        <a href="/">
+            <img src={logo} alt="Project DNA Logo" />
+        </a>
     </div>
 );
 
--- client/src/views/AccessSubPage.jsx
+++ client/src/views/AccessSubPage.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import Backgound from '../assets/img/AllAppAccess.png'; // Assuming this is the correct path to your image
 import PageHeader from '../components/PageHeader';
+import StripePaymentCard from '../assets/img/StripePaymentCard.png'; // Adjust the path as needed
 
 const AccessSubPage = () => {
     return (
-        <div>
+        <div className='access-sub-page'>
             <PageHeader />
             <h1>Membership/Subscription</h1>
             <main>
                 <img src={Backgound} alt="Description of image" />
-                <h2>All App Access</h2>
+                <div>
+                     <h2>All App Access</h2>
                 <p>Enjoy full access instantly to all apps with all the benefits of new updates, app privacy, no in-app purchases, and no advertisements.</p>
 
+                </div>
+               
+
             </main>
+            <aside>
+                <h3>Start Your Free Month</h3>
+                <div>
+                      <a href="https://buy.stripe.com/test_dRmdRb7LE9D1eWEdNY1sQ00">
+                <button className='main-btn'>Subscribe</button>
+                </a>
+                <p>All Apps Access $5.99/month.</p>
+                <img src={StripePaymentCard} alt="" />
+
+
+                </div>
+              
+
+            </aside>
         </div>
     );
 };
