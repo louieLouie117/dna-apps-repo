8966c722ae2376ddd3a2d87f5eedd4681f37355a
Luis Cardona <cardona-luis@outlook.com>
2025-11-24 10:41:09 -0600

    RenderingUserAccountStatus

--- client/src/views/DashboardUser.jsx
+++ client/src/views/DashboardUser.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import {React, useState, useEffect, use} from 'react';
 import SignOutButton from '../components/SignOutButton';
+import supabase from '../config/SupaBaseClient';
 
 const DashboardUser = () => {
     // get user ID from cookie and username
+    const [userLoggedIn, setUserLoggedIn] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
+    const [AccountStatus, setAccountStatus] = useState(null);
+
+    useEffect(() => {
+        // call fetchUserInfo when userLoggedIn changes
+        fetchUserInfo();
+    }, [userLoggedIn]);
+
+        
+
     const getUserIdFromCookie = () => {
         const match = document.cookie.match(new RegExp('(^| )userId=([^;]+)'));
         return match ? match[2] : null;
@@ -16,7 +28,27 @@ const DashboardUser = () => {
     const username = getUsernameFromCookie();
     console.log('User ID from cookie:', userId);
     console.log('Username from cookie:', username);
-    
+
+    // get user info from supabase using username
+    const fetchUserInfo = async () => {
+        if (!username) return;      
+        // Fetch user info logic here
+        const { data, error } = await supabase
+            .from('Users')
+            .select('*')
+            .eq('email', username);
+
+        if (error) {
+            console.error('Error fetching user info:', error);
+        } else {
+            console.log('User info:', data);
+            setUserInfo(data);
+            setAccountStatus(data?.[0]?.status || null);
+            console.log('-------User info state updated:', data);
+        }
+    };
+
+
     return (
         <div className="dashboard-container">
             <header style={{
@@ -30,11 +62,14 @@ const DashboardUser = () => {
                 <h1>Hello New Dashboard</h1>
                 <SignOutButton />
             </header>
+           
             
             <div className="dashboard-content">
                 <p>Welcome to your user dashboard!</p>
-                <p>User ID: {userId}</p>
                 <p>Username: {username}</p>
+                 <main>
+                <p>Account Status: {AccountStatus}</p>
+            </main>
 
             </div>
         </div>
